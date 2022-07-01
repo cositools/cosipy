9b6bff360a660d7e6913575410892c7b06e15d6c
Christopher Karwin <ckarwin@login001.palmetto.clemson.edu>
2022-07-01 09:30:24 -0400

    Initial commit of DataIO and MakePlots classes

--- cosipy/__init__.py
+++ cosipy/__init__.py
@@ -1,3 +1,7 @@
 from ._version import __version__
 
 from .response import DetectorResponse
+from .data_io import DataIO
+from .data_io import UnBinnedData
+from .data_io import BinnedData
+from .make_plots import MakePlots
---
+++ cosipy/make_plots/__init__.py
@@ -0,0 +1,1 @@
+from .MakePlots import MakePlots
