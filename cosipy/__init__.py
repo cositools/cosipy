6b7d8972d4d315995743e1f965009931e70abcac
Chris Karwin <54562666+ckarwin@users.noreply.github.com>
2023-05-05 13:50:03 +0200

    Update __init__.py

--- cosipy/__init__.py
+++ cosipy/__init__.py
@@ -5,7 +5,7 @@ from .response import DetectorResponse
 from .data_io import DataIO
 from .data_io import UnBinnedData
 from .data_io import BinnedData
-from .data_io import ReadTraTest
+#from .data_io import ReadTraTest
 from .make_plots import MakePlots
 
 from .threeml import COSILike
