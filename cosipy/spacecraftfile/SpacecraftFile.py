8e83bc7e5695ad2fe93675405c3699f99741d595
Yong2Sheng <sheng2@clemson.edu>
2023-11-17 12:21:31 -0500

    Add saving options for src path and dwell map with default to be False (not saving)

--- cosipy/spacecraftfile/SpacecraftFile.py
+++ cosipy/spacecraftfile/SpacecraftFile.py
@@ -267,7 +267,7 @@ class SpacecraftFile():
 
         return self.attitude
 
-    def get_target_in_sc_frame(self, target_name, target_coord, attitude = None, quiet = False):
+    def get_target_in_sc_frame(self, target_name, target_coord, attitude = None, quiet = False, save = False):
 
         """
         Convert the x, y and z pointings of the spacescraft axes to the path of the source in the spacecraft frame.
@@ -308,14 +308,15 @@ class SpacecraftFile():
         l = np.array(self.src_path_spherical[2].deg)  # note that 0 is Quanty, 1 is latitude and 2 is longitude and they are in rad not deg
         b = np.array(self.src_path_spherical[1].deg)
         self.src_path_lb = np.stack((l,b), axis=-1)
-        np.save(self.target_name+"_source_path_in_SC_frame", self.src_path_lb)
+        if save == True:
+            np.save(self.target_name+"_source_path_in_SC_frame", self.src_path_lb)
 
         # convert to SkyCoord objects to get the output object of this method
         self.src_path_skycoord = SkyCoord(self.src_path_lb[:,0], self.src_path_lb[:,1], unit = "deg", frame = SpacecraftFrame())
 
         return self.src_path_skycoord
 
-    def get_dwell_map(self, response, dts = None, dt_format = None, src_path = None):
+    def get_dwell_map(self, response, dts = None, dt_format = None, src_path = None, save = False):
 
         """
         Generates the dwell time map for the source.
@@ -363,7 +364,8 @@ class SpacecraftFile():
                     self.dwell_map[p] += w*(duration.unix*u.s)
 
 
-        self.dwell_map.write_map(self.target_name + "_DwellMap.fits", overwrite = True)
+        if save == True:
+            self.dwell_map.write_map(self.target_name + "_DwellMap.fits", overwrite = True)
 
         return self.dwell_map
 
