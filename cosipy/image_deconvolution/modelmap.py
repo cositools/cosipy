6df8b909da3d9be5b2b872787ca479ce7cc91fc1
Hiroki Yoneda <hiroki.yoneda.phys@gmail.com>
2023-12-15 17:33:57 +0100

    added set_values_from_extendedmodel in ModelMap class, to convert an astropy's extendedmodel to a datacube.

--- cosipy/image_deconvolution/modelmap.py
+++ cosipy/image_deconvolution/modelmap.py
@@ -2,6 +2,9 @@ import astropy.units as u
 import numpy as np
 
 from histpy import Histogram, Axes, Axis, HealpixAxis
+import astropy.units as u
+
+from cosipy.threeml.custom_functions import get_integrated_spectral_model
 
 class ModelMap(Histogram):
 
@@ -10,19 +13,21 @@ class ModelMap(Histogram):
                  energy_edges,
                  scheme = 'ring',
                  coordsys = 'galactic',
+                 label_image = 'lb',
+                 label_energy = 'Ei'
                  ):
 
         if energy_edges.unit != u.keV:
             print("Warning (ModelMap): the unit of energy_edges is not keV!")
 
-        image_axis = HealpixAxis(nside = nside,
-                                 scheme = scheme,
-                                 coordsys = coordsys,
-                                 label = "lb")
+        self.image_axis = HealpixAxis(nside = nside,
+                                      scheme = scheme,
+                                      coordsys = coordsys,
+                                      label = label_image)
 
-        energy_axis = Axis(edges = energy_edges, label = "Ei", scale = "log")
+        self.energy_axis = Axis(edges = energy_edges, label = label_energy, scale = "log")
 
-        axes = Axes([image_axis, energy_axis])
+        axes = Axes([self.image_axis, self.energy_axis])
 
         super().__init__(axes, sparse = False, unit = 1 / u.s / u.cm**2 / u.sr) # unit might be specified in the input parameter.
 
@@ -33,3 +38,15 @@ class ModelMap(Histogram):
                 self[:,idx:idx+1] = value * self.unit
     #    elif algorithm_name == ... 
     #       ...
+
+    def set_values_from_extendedmodel(self, extendedmodel):
+
+        integrated_flux = get_integrated_spectral_model(spectrum = extendedmodel.spectrum.main.shape,
+                                                        eaxis = self.energy_axis)
+        
+        npix = self.image_axis.npix
+        coords = self.image_axis.pix2skycoord(np.arange(npix))
+
+        normalized_map = extendedmodel.spatial_shape(coords.l.deg, coords.b.deg) / u.sr
+
+        self[:] = np.tensordot(normalized_map, integrated_flux.contents, axes = 0) 
