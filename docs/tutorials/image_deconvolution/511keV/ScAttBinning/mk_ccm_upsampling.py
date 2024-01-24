7cd958d1d91d7923f8c94b736cce15c196291a27
Hiroki Yoneda <hiroki.yoneda.phys@gmail.com>
2024-01-24 13:52:16 +0100

    modified the image deconvolution notebooks (including galactic CDS notebooks) and relevant files

--- docs/tutorials/image_deconvolution/511keV/ScAttBinning/mk_ccm_upsampling.py
+++ docs/tutorials/image_deconvolution/511keV/ScAttBinning/mk_ccm_upsampling.py
@@ -1,9 +1,7 @@
 from cosipy.response import FullDetectorResponse
 from cosipy.image_deconvolution import SpacecraftAttitudeExposureTable, CoordsysConversionMatrix
 
-filepath = "/Users/yoneda/Work/Exp/COSI/cosipy-2/data_challenge/DC2/prework/data/"
-
-full_detector_response_filename = filepath + "response/SMEXv12.511keV.HEALPixO4.binnedimaging.imagingresponse.nonsparse_nside16.area.h5"
+full_detector_response_filename = "/Users/yoneda/Work/Exp/COSI/cosipy-2/data_challenge/DC2/prework/data/Responses/SMEXv12.511keV.HEALPixO4.binnedimaging.imagingresponse.nonsparse_nside16.area.h5"
 full_detector_response = FullDetectorResponse.open(full_detector_response_filename)
 
 exposure_table = SpacecraftAttitudeExposureTable.from_fits("exposure_table_nside32.fits")
