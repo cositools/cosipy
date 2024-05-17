863ea23c8c1eb20c1915e419b8ba6d1cf23e9f94
Israel Martinez <imc@umd.edu>
2024-05-17 11:55:14 -0400

    Use system tmp directory for there is no leftover file from test. Add tests to have 100% coverage for file.

--- tests/util/test_data_fetching.py
+++ tests/util/test_data_fetching.py
@@ -1,10 +1,33 @@
 from cosipy.util import fetch_wasabi_file
+import tempfile
+import os
+from pathlib import Path
+import pytest
 
 def test_fetch_wasabi_file():
 
-    fetch_wasabi_file('test_file.txt', override = True)
+    with tempfile.TemporaryDirectory() as tmpdir:
 
-    f = open('test_file.txt')
+        filename = 'test_file.txt'
 
-    assert f.read() == 'Small file used for testing purposes.\n'
+        # Using output
+        output = Path(tmpdir)/filename
+        fetch_wasabi_file(filename, output = output)
+        
+        f = open(output)
+        
+        assert f.read() == 'Small file used for testing purposes.\n'
 
+        # Current directory default and override
+        os.chdir(tmpdir)
+        
+        fetch_wasabi_file(filename, override = True)
+        
+        f = open(filename)
+        
+        assert f.read() == 'Small file used for testing purposes.\n'
+
+        # Test error when file exists and no override
+        with pytest.raises(RuntimeError):
+            fetch_wasabi_file(filename)
+        
