41e44f4b1f58d37ca75a8b6cd6d8408f014122a4
Hiroki Yoneda <hiroki.yoneda.phys@gmail.com>
2024-06-03 18:24:50 +0200

    minor change in tutorials/image_deconvolution/511keV/ScAttBinning/mk_ccm_upsampling.py

--- docs/tutorials/image_deconvolution/511keV/ScAttBinning/mk_ccm_upsampling.py
+++ docs/tutorials/image_deconvolution/511keV/ScAttBinning/mk_ccm_upsampling.py
@@ -1,5 +1,6 @@
 from cosipy.response import FullDetectorResponse
 from cosipy.image_deconvolution import SpacecraftAttitudeExposureTable, CoordsysConversionMatrix
+import astropy.units as u
 
 full_detector_response_filename = "/Users/yoneda/Work/Exp/COSI/cosipy-2/data_challenge/DC2/prework/data/Responses/SMEXv12.511keV.HEALPixO4.binnedimaging.imagingresponse.nonsparse_nside16.area.h5" # Please replace this with your file path
 full_detector_response = FullDetectorResponse.open(full_detector_response_filename)
@@ -7,4 +8,6 @@ full_detector_response = FullDetectorResponse.open(full_detector_response_filena
 exposure_table = SpacecraftAttitudeExposureTable.from_fits("exposure_table_nside32.fits")
 
 coordsys_conv_matrix = CoordsysConversionMatrix.spacecraft_attitude_binning_ccm(full_detector_response, exposure_table, nside_model = 32, use_averaged_pointing = True)
+# if you want to consider the earth occultation effect:
+#coordsys_conv_matrix = CoordsysConversionMatrix.spacecraft_attitude_binning_ccm(full_detector_response, exposure_table, nside_model = 32, use_averaged_pointing = True, earth_horizon_angle = 113.0 * u.deg)
 coordsys_conv_matrix.write("ccm_nside32.hdf5", overwrite = True)
