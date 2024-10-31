f280b1d74aa15f89900f0cbba05cc5faa3cab102
ckarwin <ckarwin@vm-slurm-p-login01.palmetto.clemson.edu>
2024-10-31 18:14:39 -0400

    updated unit test and added test files

--- tests/background_estimation/test_continuum_background_estimation.py
+++ tests/background_estimation/test_continuum_background_estimation.py
@@ -4,13 +4,13 @@ from cosipy import test_data
 
 def test_continuum_background_estimation():
    
+    
     instance = ContinuumEstimation() 
      
     # Test main method:
-    #data_file = test_data.path / "bkg_pl.h5"
-    data_file = "/project/majello/astrohe/ckarwin/COSI/COSIpy_Development/Continuum_BG_Estimation/Run_8/crab_bkg_binned_data_galactic.hdf5"
-    data_yaml = test_data.path / "inputs_crab.yaml"
-    #psr_file = test_data.path / "test_precomputed_response.h5"
-    psr_file = "/project/majello/astrohe/ckarwin/COSI/COSIpy_Development/Continuum_BG_Estimation/Run_8/crab_psr.h5"
+    data_yaml = test_data.path / "inputs_crab_continuum_bg_estimation_testing.yaml"
+    data_file = test_data.path / "crab_bkg_binned_data_for_continuum_bg_testing.hdf5"
+    psr_file = test_data.path / "test_precomputed_response.h5"
     psr = instance.load_psr_from_file(psr_file)
-    instance.continuum_bg_estimation(data_file, data_yaml, psr, e_loop=(2,3), s_loop=(4,5))
+    
+    instance.continuum_bg_estimation(data_file, data_yaml, psr, e_loop=(1,2), s_loop=(1,2))
