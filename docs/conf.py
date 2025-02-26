9f036577e74f8a9abf44b2337cde7cba7e88da89
Chris Karwin <54562666+ckarwin@users.noreply.github.com>
2025-02-26 11:24:38 -0500

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -38,6 +38,8 @@ extensions = ['sphinx.ext.mathjax',
               'nbsphinx'
 ]
 
+highlight_language = 'python3'
+
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
 
