a07823cb7b6837399dbf7e23c6e2f35be3518e85
Eliza Neights <eneights@gmail.com>
2025-03-11 15:07:01 -0400

    Add from_scattering_direction() to polarization_angle

--- cosipy/polarization/polarization_angle.py
+++ cosipy/polarization/polarization_angle.py
@@ -100,4 +100,79 @@ class PolarizationAngle:
                                  self.skycoord,
                                  convention = convention2)
 
+    @classmethod
+    def from_scattering_direction(cls, psichi, source_coord, convention):
+        """
+        Calculate the azimuthal scattering angle of a scattered photon.
+        
+        Parameters
+        ----------
+        psichi : astropy.coordinates.SkyCoord
+            Scattered photon direction
+        source_coord : astropy.coordinates.SkyCoord
+            Source direction
+        convention :
+            cosipy.polarization.PolarizationConvention
+
+        Returns
+        -------
+        azimuthal_scattering_angle : cosipy.polarization.PolarizationAngle
+            Azimuthal scattering angle
+        """
+
+        if not psichi.frame.name == source_coord.frame.name:
+            raise RuntimeError('psichi and source_coord must have the same frame')
+
+        reference_coord = convention.get_basis(source_coord)[0]
+
+        source_vector_cartesian = [source_coord.cartesian.x.value,
+                                   source_coord.cartesian.y.value, 
+                                   source_coord.cartesian.z.value]
+
+        reference_vector_cartesian = [reference_coord.cartesian.x.value, 
+                                      reference_coord.cartesian.y.value,
+                                      reference_coord.cartesian.z.value]
+
+        if psichi.frame.name == 'spacecraftframe':
+
+            psi = (np.pi/2) - psichi.lat.rad
+            chi = psichi.lon.rad
+
+        elif psichi.frame.name == 'galactic':
+
+            psi = (np.pi/2) - psichi.b.rad
+            chi = psichi.l.rad
+
+        elif psichi.frame.name == 'icrs':
+
+            psi = (np.pi/2) - psichi.dec.rad
+            chi = psichi.ra.rad
+
+        else:
+            raise RuntimeError('Unsupported frame "' + psichi.frame.name + '"')
+
+        # Convert scattered photon vector from spherical to Cartesian coordinates
+        scattered_photon_vector = [np.sin(psi) * np.cos(chi), np.sin(psi) * np.sin(chi), np.cos(psi)]
+
+        # Project scattered photon vector onto plane perpendicular to source direction
+        d = np.dot(scattered_photon_vector, source_vector_cartesian) / np.dot(source_vector_cartesian, source_vector_cartesian)
+        projection = [scattered_photon_vector[0] - (d * source_vector_cartesian[0]), 
+                      scattered_photon_vector[1] - (d * source_vector_cartesian[1]), 
+                      scattered_photon_vector[2] - (d * source_vector_cartesian[2])]
+
+        # Calculate angle between scattered photon vector & reference vector on plane perpendicular to source direction
+        cross_product = np.cross(projection, reference_vector_cartesian)
+        if np.dot(source_vector_cartesian, cross_product) < 0:
+            sign = -1
+        else:
+            sign = 1
+        normalization = np.sqrt(np.dot(projection, projection)) * np.sqrt(np.dot(reference_vector_cartesian, reference_vector_cartesian))
+    
+        angle = Angle(sign * np.arccos(np.dot(projection, reference_vector_cartesian) / normalization), unit=u.rad)
+
+        azimuthal_scattering_angle = cls(angle, source_coord, convention=convention)
+
+        return azimuthal_scattering_angle
+
+
         
