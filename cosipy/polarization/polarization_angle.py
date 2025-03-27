Correct polarization angle rotation

cosipy/polarization/polarization_angle.py
@@ -1,6 +1,7 @@
 import numpy as np
 from astropy.coordinates import SkyCoord, Angle
 import astropy.units as u
+from scoords import SpacecraftFrame
 
 from .conventions import PolarizationConvention
 
@@ -120,39 +121,30 @@ class PolarizationAngle:
             Azimuthal scattering angle
         """
 
-        if not psichi.frame.name == source_coord.frame.name:
-            raise RuntimeError('psichi and source_coord must have the same frame')
+        if convention.frame.name == 'spacecraftframe':
+
+            source_coord = source_coord.transform_to(SpacecraftFrame(attitude=source_coord.attitude))
+            psichi = psichi.transform_to(SpacecraftFrame(attitude=source_coord.attitude))
+
+        elif convention.frame.name == 'icrs':
+
+            source_coord = source_coord.transform_to('icrs')
+            psichi = psichi.transform_to('icrs')
+
+        elif convention.frame.name == 'galactic':
+
+            source_coord = source_coord.transform_to('galactic')
+            psichi = psichi.transform_to('galactic')
+
+        else:
+
+            raise RuntimeError('Unsupported polarization convention frame "' + convention.frame.name + '"')
 
         reference_coord = convention.get_basis(source_coord)[0]
 
-        source_vector_cartesian = [source_coord.cartesian.x.value,
-                                   source_coord.cartesian.y.value, 
-                                   source_coord.cartesian.z.value]
-
-        reference_vector_cartesian = [reference_coord.cartesian.x.value, 
-                                      reference_coord.cartesian.y.value,
-                                      reference_coord.cartesian.z.value]
-
-        if psichi.frame.name == 'spacecraftframe':
-
-            psi = (np.pi/2) - psichi.lat.rad
-            chi = psichi.lon.rad
-
-        elif psichi.frame.name == 'galactic':
-
-            psi = (np.pi/2) - psichi.b.rad
-            chi = psichi.l.rad
-
-        elif psichi.frame.name == 'icrs':
-
-            psi = (np.pi/2) - psichi.dec.rad
-            chi = psichi.ra.rad
-
-        else:
-            raise RuntimeError('Unsupported frame "' + psichi.frame.name + '"')
-
-        # Convert scattered photon vector from spherical to Cartesian coordinates
-        scattered_photon_vector = [np.sin(psi) * np.cos(chi), np.sin(psi) * np.sin(chi), np.cos(psi)]
+        source_vector_cartesian = source_coord.cartesian.xyz.value
+        reference_vector_cartesian = reference_coord.cartesian.xyz.value
+        scattered_photon_vector = psichi.cartesian.xyz.value
 
         # Project scattered photon vector onto plane perpendicular to source direction
         d = np.dot(scattered_photon_vector, source_vector_cartesian) / np.dot(source_vector_cartesian, source_vector_cartesian)

cosipy/response/PointSourceResponse.py
@@ -2,11 +2,7 @@ from histpy import Histogram#, Axes, Axis
 
 import numpy as np
 import astropy.units as u
-#from astropy.units import Quantity
-#from scipy import integrate
 from scoords import SpacecraftFrame, Attitude
-
-#from threeML import DiracDelta, Constant, Line, Quadratic, Cubic, Quartic, StepFunction, StepFunctionUpper, Cosine_Prior, Uniform_prior, PhAbs, Gaussian
 
 from .functions import get_integrated_spectral_model
 
@@ -47,7 +43,7 @@ class PointSourceResponse(Histogram):
         
         return self.axes['Ei']
        
-    def get_expectation(self, spectrum, polarization_level=None, polarization_angle=None, scatt_map=None, convention=None):
+    def get_expectation(self, spectrum, polarization=None):
         """
         Convolve the response with a spectral (and optionally, polarization) hypothesis to obtain the expected
         excess counts from the source.
@@ -56,14 +52,8 @@ class PointSourceResponse(Histogram):
         ----------
         spectrum : :py:class:`threeML.Model`
             Spectral hypothesis.
-        polarization_level : float, optional
-            Polarization level (between 0 and 1).
-        polarization_angle : :py:class:`cosipy.polarization.polarization_angle.PolarizationAngle`, optional
-            Polarization angle. If in the spacecraft frame, the angle must have the same convention as the response.
-        scatt_map : :py:class:`cosipy.spacecraftfile.scatt_map.SpacecraftAttitudeMap`, optional
-            Spacecraft attitude map. Only needed for polarization angle provided in galactic reference frame.
-        convention : str, optional
-            Polarization angle convention of response ('RelativeX', 'RelativeY', or 'RelativeZ'). Only needed for polarization angle provided in galactic reference frame.
+        polarization : 'astromodels.core.polarization.LinearPolarization', optional
+            Polarization angle and degree. The angle is assumed to have same convention as point source response.
         
         Returns
         -------
@@ -71,130 +61,38 @@ class PointSourceResponse(Histogram):
              Histogram with the expected counts on each analysis bin
         """
 
-        if polarization_level == None:
+        if polarization is None:
 
-            if polarization_angle == None:
+            if 'Pol' in self.axes.labels:
 
-                if 'Pol' in self.axes.labels:
+                raise RuntimeError("Must include polarization in point source response if using polarization response")
 
-                    raise RuntimeError("Must include polarization in point source response if using polarization response")
-
-                else:
-
-                    contents = self.contents
-                    axes = self.axes[1:]
-
-            else:
-
-                raise RuntimeError("Must provide both polarization level and angle")
-
-        elif polarization_angle != None:
-
-            from cosipy.polarization.polarization_angle import PolarizationAngle
-            from cosipy.polarization.conventions import MEGAlibRelativeX, MEGAlibRelativeY, MEGAlibRelativeZ, IAUPolarizationConvention
-
-            if polarization_level > 1.0 or polarization_level < 0:
-
-                raise RuntimeError("Polarization level must be a fraction between 0 and 1")
-
-            elif not 'Pol' in self.axes.labels:
-                
-                raise RuntimeError("Response must have polarization angle axis to include polarization in point source response")
-
-            elif type(polarization_angle.convention.frame) == SpacecraftFrame:
-
-                logger.warning("The response must have the same polarization convention as the provided polarization angle, or the result will be incorrect!")
-
-                if polarization_angle.angle.deg == 180.:
-                    polarization_angle = PolarizationAngle(0. * u.deg, polarization_angle.skycoord, convention=polarization_angle.convention)
-
-                polarization_angle_components = []
-
-                for i in range(self.axes['Pol'].nbins):
-
-                    polarization_angle_components.append(self.slice[{'Pol':slice(i,i+1)}].project('Ei', 'Em', 'Phi', 'PsiChi'))
-
-                    if polarization_angle.angle.deg >= self.axes['Pol'].edges.to_value(u.deg)[i] and polarization_angle.angle.deg < self.axes['Pol'].edges.to_value(u.deg)[i+1]:
-                        polarized_component = polarization_angle_components[i].contents
-
-                unpolarized_component = polarization_angle_components[0].contents
-
-                for i in range(len(polarization_angle_components) - 1):
-
-                    unpolarized_component += polarization_angle_components[i+1].contents
-
-                polarized_component /= np.sum(polarized_component.value)
-                unpolarized_component /= np.sum(unpolarized_component.value)
-
-                polarization_hist = (polarization_level * polarized_component) + ((1 - polarization_level) * unpolarized_component)
-                polarization_hist *= np.sum(self.contents) / np.sum(polarization_hist)
-
-                contents = polarization_hist
-                axes = self.project('Ei', 'Em', 'Phi', 'PsiChi').axes[1:]
-
-            elif type(scatt_map) != None and convention != None:
-
-                polarization_angle_components = np.empty(self.axes['Pol'].nbins, dtype=Histogram)
-
-                if polarization_angle.angle.deg == 180.:
-                    polarization_angle = PolarizationAngle(0. * u.deg, polarization_angle.skycoord, convention=polarization_angle.convention)
-
-                for i in range(self.axes['Pol'].nbins):
-
-                    response_slice = self.slice[{'Pol':slice(i,i+1)}].project('Ei', 'Em', 'Phi', 'PsiChi')
-
-                    for j, (pixels, exposure) in enumerate(zip(scatt_map.contents.coords.transpose(), scatt_map.contents.data)):
-
-                        attitude = Attitude.from_axes(x=scatt_map.axes['x'].pix2skycoord(pixels[0]), y=scatt_map.axes['y'].pix2skycoord(pixels[1]))
-
-                        if convention == 'RelativeX':
-                            this_convention = MEGAlibRelativeX(attitude=attitude)
-                        elif convention == 'RelativeY':
-                            this_convention = MEGAlibRelativeY(attitude=attitude)
-                        elif convention == 'RelativeZ':
-                            this_convention = MEGAlibRelativeZ(attitude=attitude)
-                        else:
-                            raise RuntimeError("Response convention must be 'RelativeX', 'RelativeY', or 'RelativeZ'")
-
-                        polarization_angle_galactic = PolarizationAngle(self.axes['Pol'].centers.to_value(u.deg)[i] * u.deg, polarization_angle.skycoord, convention=this_convention).transform_to(polarization_angle.convention)
-
-                        if polarization_angle_galactic.angle.deg == 180.:
-                            polarization_angle_galactic = PolarizationAngle(0. * u.deg, polarization_angle_galactic.skycoord, convention=polarization_angle_galactic.convention)
-
-                        polarization_angle_index = np.max(np.where(polarization_angle_galactic.angle.deg >= self.axes['Pol'].edges.to_value(u.deg)))
-
-                        if hasattr(polarization_angle_components[polarization_angle_index], 'axes'):
-                            polarization_angle_components[polarization_angle_index] += response_slice * exposure / np.sum(scatt_map.project('x'))
-                        else:
-                            polarization_angle_components[polarization_angle_index] = response_slice * exposure / np.sum(scatt_map.project('x'))
-
-                for i in range(self.axes['Pol'].nbins):
-
-                    if polarization_angle.angle.deg >= self.axes['Pol'].edges.to_value(u.deg)[i] and polarization_angle.angle.deg < self.axes['Pol'].edges.to_value(u.deg)[i+1]:
-                        polarized_component = polarization_angle_components[i].contents
-
-                unpolarized_component = polarization_angle_components[0].contents
-
-                for i in range(len(polarization_angle_components) - 1):
-
-                    unpolarized_component += polarization_angle_components[i+1].contents
-
-                polarized_component /= np.sum(polarized_component.value)
-                unpolarized_component /= np.sum(unpolarized_component.value)
-
-                polarization_hist = (polarization_level * polarized_component) + ((1 - polarization_level) * unpolarized_component)
-                polarization_hist *= np.sum(self.contents) / np.sum(polarization_hist)
-
-                contents = polarization_hist
-                axes = self.project('Ei', 'Em', 'Phi', 'PsiChi').axes[1:]
-
-            else:
-
-                raise RuntimeError("Scatt map and response convention must be provided to include polarization in galactic coordinates in point source response")
+            contents = self.contents
+            axes = self.axes[1:]
 
         else:
 
-            raise RuntimeError("Must provide both polarization level and angle to include polarization in point source response")
+            if not 'Pol' in self.axes.labels:
+                
+                raise RuntimeError("Response must have polarization angle axis to include polarization in point source response")
+
+            polarization_angle = polarization.angle.value
+            polarization_level = polarization.degree.value / 100.
+
+            if polarization_angle == 180.:
+                polarization_angle = 0.
+
+            unpolarized_weights = np.full(self.axes['Pol'].nbins, (1. - polarization_level) / self.axes['Pol'].nbins)
+            polarized_weights = np.zeros(self.axes['Pol'].nbins)
+
+            polarization_bin_index = self.axes['Pol'].find_bin(polarization_angle * u.deg)
+            polarized_weights[polarization_bin_index] = polarization_level
+
+            weights = unpolarized_weights + polarized_weights
+
+            contents = np.tensordot(weights, self.contents, axes=([0], [self.axes.label_to_index('Pol')]))
+
+            axes = self.axes['Em', 'Phi', 'PsiChi']
 
         energy_axis = self.photon_energy_axis
 