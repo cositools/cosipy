fb155ac9917dde4e566b36b6bc83dc7b3ea02399
ldigesu <150834542+ldigesu@users.noreply.github.com>
2025-03-31 12:18:04 +0200

    commit of test_pipeline.py

--- tests/pipeline/test_pipeline.py
+++ tests/pipeline/test_pipeline.py
@@ -1,4 +1,7 @@
 import os
+from cosipy import test_data
+#
+data_path = str(test_data.path)
 os.system("cosi-threemlfit --config test_spec_grb.yaml")
-assert os.path.exists("../../cosipy/test_data/grb_test.fits")
-assert os.path.exists("../../cosipy/test_data/fit_grb_test.pdf")
+assert os.path.exists(str(data_path+"/grb_test.fits"))
+assert os.path.exists(str(data_path+"/fit_grb_test.pdf"))
