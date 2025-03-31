7d514725c2b1141ca94e236dab42b47668882836
ldigesu <150834542+ldigesu@users.noreply.github.com>
2025-03-31 15:40:34 +0200

    add different naming for test

--- tests/pipeline/test_pipeline.py
+++ tests/pipeline/test_pipeline.py
@@ -22,6 +22,8 @@ data_path = str(test_data.path)
 config_tmp_path=str(data_path+"/test_spec_grb_tmp.yaml")
 config_path=str(data_path+"/test_spec_grb.yaml")
 replace_text_in_file(config_tmp_path,"TDATAPATH",data_path,config_path)
-os.system(str("cosi-threemlfit --config "+config_path))
-assert os.path.exists(str(data_path+"/grb_test.fits"))
-assert os.path.exists(str(data_path+"/fit_grb_test.pdf"))
+#
+def test_output_ex():
+    os.system(str("cosi-threemlfit --config "+config_path))
+    assert os.path.exists(str(data_path+"/grb_test.fits"))
+    assert os.path.exists(str(data_path+"/fit_grb_test.pdf"))
