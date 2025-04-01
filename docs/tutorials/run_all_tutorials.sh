c1a9d84450f2e04107f43b5da44f1aff69ee987e
Israel Martinez <imc@umd.edu>
2025-04-01 14:59:09 -0400

    working version of check all tutorials

---
+++ docs/tutorials/run_all_tutorials.sh
@@ -0,0 +1,12 @@
+#/usr/bin/env bash
+
+# Local mirror
+cosi_pipeline_public="${HOME}/cosi/data/wasabi/cosi-pipelines-public"
+
+#ln -s "${cosi_pipeline_public}/COSI-SMEX/DC2/Data/Orientation/20280301_3_month.ori" DataIO
+#(cd DataIO && jupyter nbconvert --to html --execute --ExecutePreprocessor.kernel_name=python3 DataIO_example.ipynb)
+
+ln -s "${cosi_pipeline_public}/COSI-SMEX/DC2/Data/Orientation/20280301_3_month.ori" response
+ln -s "${cosi_pipeline_public}/COSI-SMEX/DC2/Responses/SMEXv12.Continuum.HEALPixO3_10bins_log_flat.binnedimaging.imagingresponse.nonsparse_nside8.area.good_chunks_unzip.h5.zip" response
+ln -s "${cosi_pipeline_public}/COSI-SMEX/DC2/Responses/SMEXv12.Continuum.HEALPixO3_10bins_log_flat.binnedimaging.imagingresponse.nonsparse_nside8.area.good_chunks_unzip.h5" response
+(cd response && jupyter nbconvert --to html --execute --ExecutePreprocessor.kernel_name=python3 SpacecraftFile.ipynb)
