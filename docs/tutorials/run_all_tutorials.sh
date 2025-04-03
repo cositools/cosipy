7db45f9cda125ee418799168e39838d415347448
Israel Martinez <imc@umd.edu>
2025-04-03 15:56:56 -0400

    Add source injector to autorun

--- docs/tutorials/run_all_tutorials.sh
+++ docs/tutorials/run_all_tutorials.sh
@@ -63,24 +63,29 @@ cosi_pipeline_public="${HOME}/cosi/data/wasabi/cosi-pipelines-public"
 #ln -fs "${cosi_pipeline_public}/COSI-SMEX/cosipy_tutorials/extended_source_spectral_fit_galactic_frame/combined_binned_data_thin_disk.hdf5" $wdir
 #(cd $wdir && jupyter nbconvert --to html --execute --ExecutePreprocessor.kernel_name=${kernel_name} diffuse_511_spectral_fit.ipynb)
 
-wdir=image_deconvolution/511keV/ScAttBinning
+#wdir=image_deconvolution/511keV/ScAttBinning
+#
+## Will fix this when we move to use clean temporary directories
+#for f in 511keV_dc2_scatt_bkg.hdf5 511keV_dc2_scatt_signal.hdf5 511keV_dc2_scatt_event.hdf5
+#  do
+#    if [ -e "${wdir}/${f}" ]; then
+#      rm "${wdir}/${f}"
+#    fi
+#  done
+#
+#ln -fs "${cosi_pipeline_public}/COSI-SMEX/DC2/Responses/SMEXv12.511keV.HEALPixO4.binnedimaging.imagingresponse.nonsparse_nside16.area.h5" $wdir
+#ln -fs "${cosi_pipeline_public}/COSI-SMEX/DC2/Data/Sources/511_thin_disk_3months_unbinned_data.fits.gz" $wdir
+#ln -fs "${cosi_pipeline_public}/COSI-SMEX/DC2/Data/Backgrounds/albedo_photons_3months_unbinned_data.fits.gz" $wdir
+#ln -fs "${cosi_pipeline_public}/COSI-SMEX/DC2/Data/Orientation/20280301_3_month_with_orbital_info.ori" $wdir
+#(cd $wdir &&
+#jupyter nbconvert --to html --execute --ExecutePreprocessor.kernel_name=${kernel_name} 511keV-ScAtt-DataReduction.ipynb &&
+#jupyter nbconvert --to html --execute --ExecutePreprocessor.kernel_name=${kernel_name} 511keV-ScAtt-ImageDeconvolution.ipynb)
 
-# Will fix this when we move to use clean temporary directories
-for f in 511keV_dc2_scatt_bkg.hdf5 511keV_dc2_scatt_signal.hdf5 511keV_dc2_scatt_event.hdf5
-  do
-    if [ -e "${wdir}/${f}" ]; then
-      rm "${wdir}/${f}"
-    fi
-  done
 
-ln -fs "${cosi_pipeline_public}/COSI-SMEX/DC2/Responses/SMEXv12.511keV.HEALPixO4.binnedimaging.imagingresponse.nonsparse_nside16.area.h5" $wdir
-ln -fs "${cosi_pipeline_public}/COSI-SMEX/DC2/Data/Sources/511_thin_disk_3months_unbinned_data.fits.gz" $wdir
-ln -fs "${cosi_pipeline_public}/COSI-SMEX/DC2/Data/Backgrounds/albedo_photons_3months_unbinned_data.fits.gz" $wdir
+wdir=source_injector
+ln -fs "${cosi_pipeline_public}/COSI-SMEX/DC2/Responses/SMEXv12.Continuum.HEALPixO3_10bins_log_flat.binnedimaging.imagingresponse.nonsparse_nside8.area.good_chunks_unzip.earthocc.zip" $wdir
+ln -fs "${cosi_pipeline_public}/COSI-SMEX/DC2/Responses/SMEXv12.Continuum.HEALPixO3_10bins_log_flat.binnedimaging.imagingresponse.nonsparse_nside8.area.good_chunks_unzip.earthocc.h5" $wdir
 ln -fs "${cosi_pipeline_public}/COSI-SMEX/DC2/Data/Orientation/20280301_3_month_with_orbital_info.ori" $wdir
-(cd $wdir &&
-jupyter nbconvert --to html --execute --ExecutePreprocessor.kernel_name=${kernel_name} 511keV-ScAtt-DataReduction.ipynb &&
-jupyter nbconvert --to html --execute --ExecutePreprocessor.kernel_name=${kernel_name} 511keV-ScAtt-ImageDeconvolution.ipynb)
-
 
 
 
