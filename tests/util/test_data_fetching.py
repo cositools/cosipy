8fe3e848068f6e2ab780d8a6c32342c756fd7519
Israel Martinez <imc@umd.edu>
2025-04-07 14:27:28 -0400

    - Add tests to fetch_wasabi_file. Mostly covered now. - Move tests files in wasabi to their own folder.
    Signed-off-by: Israel Martinez <imc@umd.edu>

--- tests/util/test_data_fetching.py
+++ tests/util/test_data_fetching.py
@@ -1,4 +1,6 @@
-from cosipy.util import fetch_wasabi_file
+from typing_extensions import override
+
+from cosipy.util import fetch_wasabi_file, fetch_wasabi_file_header
 import tempfile
 import os
 from pathlib import Path
@@ -8,22 +10,22 @@ def test_fetch_wasabi_file():
 
     with tempfile.TemporaryDirectory() as tmpdir:
 
-        filename = 'test_file.txt'
+        filename = 'test_files/test_file.txt'
 
         # Using output
-        output = Path(tmpdir)/filename
+        output = Path(tmpdir)/"test_file.txt"
         fetch_wasabi_file(filename, output = output)
         
         f = open(output)
         
         assert f.read() == 'Small file used for testing purposes.\n'
 
-        # Current directory default and override
+        # Current directory default and overite
         os.chdir(tmpdir)
         
         fetch_wasabi_file(filename, overwrite= True)
         
-        f = open(filename)
+        f = open(output)
         
         assert f.read() == 'Small file used for testing purposes.\n'
 
@@ -34,4 +36,42 @@ def test_fetch_wasabi_file():
 
         with pytest.raises(RuntimeError):
             fetch_wasabi_file(filename)
-        
+
+        # Succeeds with overwrite
+        fetch_wasabi_file(filename, overwrite = True)
+
+        # Zipped file
+        fetch_wasabi_file(filename + ".zip", overwrite=True, unzip = True)
+        f = open(output)
+        assert f.read() == 'Small file used for testing purposes.\n'
+
+        fetch_wasabi_file(filename + ".gz", overwrite=True, unzip = True, unzip_output = output)
+        f = open(output)
+        assert f.read() == 'Small file used for testing purposes.\n'
+
+        # Checksum zipped file
+        fetch_wasabi_file(filename + ".gz", unzip=True, checksum = 'c29015230d84e5e44e773c51c99b5911')
+
+        # Fails if checksum not provided, file exists and overwrite = False
+        with pytest.raises(RuntimeError):
+            fetch_wasabi_file(filename + ".gz", unzip=True)
+
+        # Test unzipped file is different than checksum
+        file = open(output, "a")
+        file.write("Append test line.\n")
+        file.close()
+
+        # Fails if overwrite is False
+        with pytest.raises(RuntimeError):
+            fetch_wasabi_file(filename + ".gz", unzip=True, checksum='c29015230d84e5e44e773c51c99b5911')
+
+        # Succeeds if file is different, but overwrite = True
+        fetch_wasabi_file(filename + ".gz", unzip=True, checksum = 'c29015230d84e5e44e773c51c99b5911', overwrite = True)
+
+        # For multipart uploaded files
+        fetch_wasabi_file('test_files/test_multipart_file.txt')
+        fetch_wasabi_file('test_files/test_multipart_file.txt') # Already exists, but it's the same file, so it should succeed
+
+        # Fetch only header
+        hdr = fetch_wasabi_file_header(filename)
+        assert hdr["ETag"] == '"c29015230d84e5e44e773c51c99b5911"'
