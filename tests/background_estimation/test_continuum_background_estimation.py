d2b0ae96d3831ae87741655297d2fd55087249f3
Jeremy Buhler <jbuhler@cassini.seas.wustl.edu>
2026-02-16 23:00:34 -0600

    fix test case to avoid using removed helper functions

--- tests/background_estimation/test_continuum_background_estimation.py
+++ tests/background_estimation/test_continuum_background_estimation.py
@@ -1,16 +1,17 @@
 import pytest
 from cosipy.background_estimation import ContinuumEstimation
+from cosipy.response import DetectorResponse
 from cosipy import test_data
 
 def test_continuum_background_estimation():
-   
-    
-    instance = ContinuumEstimation() 
-     
+
+
+    instance = ContinuumEstimation()
+
     # Test main method:
     data_yaml = test_data.path / "inputs_crab_continuum_bg_estimation_testing.yaml"
     data_file = test_data.path / "crab_bkg_binned_data_for_continuum_bg_testing.hdf5"
     psr_file = test_data.path / "test_precomputed_response.h5"
-    psr = instance.load_psr_from_file(psr_file)
-    
+    psr = DetectorResponse.open(psr_file)
+
     instance.continuum_bg_estimation(data_file, data_yaml, psr, e_loop=(1,2), s_loop=(1,2))
