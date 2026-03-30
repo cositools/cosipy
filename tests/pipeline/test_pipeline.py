ed49820f536ed5f483cd15705503f8a329c5a3e3
ldigesu <150834542+ldigesu@users.noreply.github.com>
2026-03-30 18:01:12 +0200

    rm usage of os.system from test_pipeline.py

--- tests/pipeline/test_pipeline.py
+++ tests/pipeline/test_pipeline.py
@@ -1,10 +1,17 @@
 import os
 from cosipy import test_data
+from cosipy.pipeline.task.task import cosi_bindata,cosi_threemlfit
 
 data_path=str(test_data.path)
 config_path=os.path.join(data_path,"test_pipeline.yaml")
 
 def test_run_task(tmp_path):
     tmpdir = tmp_path.as_posix()
-    os.system(str("cosi-bindata --config "+ config_path + " -o " + tmpdir + " --overwrite"))
-    os.system(str("cosi-threemlfit --config "+ config_path + " -o " + tmpdir + " --overwrite"))
+    args = ['--config', config_path, '--overwrite', '--output-dir', tmpdir]
+    cosi_bindata(argv=args)
+    cosi_threemlfit(argv=args)
+    #
+    #os.system(str("cosi-bindata --config "+ config_path + " -o " + tmpdir + " --overwrite"))
+    #os.system(str("cosi-threemlfit --config "+ config_path + " -o " + tmpdir + " --overwrite"))
+
+test_run_task(test_data.path)
