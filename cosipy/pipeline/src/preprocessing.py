ac008d5ec4a80142e5bd703b401bb189cb413dd2
ldigesu <150834542+ldigesu@users.noreply.github.com>
2026-05-15 16:22:39 +0200

    Add sparse=True to cosi-bindata thus pipeline_zero example works with large dataset

--- cosipy/pipeline/src/preprocessing.py
+++ cosipy/pipeline/src/preprocessing.py
@@ -64,6 +64,6 @@ def get_binned_data(yaml_path, udata_path, bdata_name, psichi_coo):
     """
     data = BinnedData(yaml_path)
     data.get_binned_data(unbinned_data=udata_path, output_name=bdata_name, psichi_binning=psichi_coo,
-                         make_binning_plots=False)
+                         make_binning_plots=False, sparse=True)
     return ()
 
