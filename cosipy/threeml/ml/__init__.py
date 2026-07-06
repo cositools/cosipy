767d13e25ee1ce80d90250a16d003567a153ff9d
GallegoSav <123730578+GallegoSav@users.noreply.github.com>
2026-07-06 16:04:51 +0200

    Import UnbinnedThreeMLExtendedSourceResponseIRFAdaptive

--- cosipy/threeml/ml/__init__.py
+++ cosipy/threeml/ml/__init__.py
@@ -4,3 +4,4 @@ if not cosipy.with_ml:
     raise ImportError("Install cosipy with [ml] optional packages to use these features.")
 
 from .optimized_unbinned_folding import UnbinnedThreeMLPointSourceResponseIRFAdaptive
+from .optimized_unbinned_folding_extendedsource import UnbinnedThreeMLExtendedSourceResponseIRFAdaptive
