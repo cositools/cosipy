0ee42cab9e44b8c82f03ffb31be033ee5fd1413e
GallegoSav <123730578+GallegoSav@users.noreply.github.com>
2026-07-08 18:05:18 +0200

    Import scipy.integrate for numerical integration

--- cosipy/threeml/custom_functions.py
+++ cosipy/threeml/custom_functions.py
@@ -27,6 +27,8 @@ from astromodels.sources.source import Source, SourceType
 from astromodels.utils.logging import setup_logger
 from astromodels.utils.pretty_list import dict_to_list
 from typing import Optional, Dict 
+import scipy.integrate as sp_int
+
 
 import logging
 logger = logging.getLogger(__name__)
