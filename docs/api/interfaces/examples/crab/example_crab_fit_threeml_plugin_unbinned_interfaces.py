fe0b2f9446c910128734dd5ae1ee8f63e881d69d
GallegoSav <123730578+GallegoSav@users.noreply.github.com>
2026-07-31 14:58:00 +0200

    Import PointSource from cosipy.threeml.custom_functions

--- docs/api/interfaces/examples/crab/example_crab_fit_threeml_plugin_unbinned_interfaces.py
+++ docs/api/interfaces/examples/crab/example_crab_fit_threeml_plugin_unbinned_interfaces.py
@@ -46,7 +46,9 @@ from astropy.coordinates import SkyCoord, Galactic, Angle, UnitSphericalRepresen
 import numpy as np
 import matplotlib.pyplot as plt
 
-from threeML import Band, PointSource, Model, JointLikelihood, DataList
+from threeML import Band, Model, JointLikelihood, DataList
+from cosipy.threeml.custom_functions import PointSource
+
 from astromodels import Parameter, Powerlaw
 
 from pathlib import Path
