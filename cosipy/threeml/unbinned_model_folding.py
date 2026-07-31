8079e41ec446e29cc866dad5228a2d78e4d8e5ee
GallegoSav <123730578+GallegoSav@users.noreply.github.com>
2026-07-31 15:16:38 +0200

    Refactor imports in unbinned_model_folding.py

--- cosipy/threeml/unbinned_model_folding.py
+++ cosipy/threeml/unbinned_model_folding.py
@@ -2,8 +2,8 @@ import itertools
 from typing import Optional, Iterable, Union
 
 import numpy as np
-from astromodels import Model
-from cosipy.threeml.custom_functions import PointSource, ExtendedSource
+from astromodels import Model, PointSource, ExtendedSource
+from cosipy.threeml.custom_functions import CosipyPointSource, CosipyExtendedSource
 from pathlib import Path
 
 from cosipy.interfaces import UnbinnedThreeMLModelFoldingInterface, UnbinnedThreeMLSourceResponseInterface
