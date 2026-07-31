d9c572e874c1dfa0d0a471a5e06222c28f4fa52b
GallegoSav <123730578+GallegoSav@users.noreply.github.com>
2026-07-31 15:14:03 +0200

    Refactor import statements in example_crab_fit file

--- docs/api/interfaces/examples/crab/example_crab_fit_threeml_plugin_unbinned_interfaces.py
+++ docs/api/interfaces/examples/crab/example_crab_fit_threeml_plugin_unbinned_interfaces.py
@@ -46,8 +46,7 @@ from astropy.coordinates import SkyCoord, Galactic, Angle, UnitSphericalRepresen
 import numpy as np
 import matplotlib.pyplot as plt
 
-from threeML import Band, Model, JointLikelihood, DataList
-from cosipy.threeml.custom_functions import PointSource
+from threeML import Band, Model, PointSource, JointLikelihood, DataList
 
 from astromodels import Parameter, Powerlaw
 
