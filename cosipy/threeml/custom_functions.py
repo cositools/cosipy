42e7178dffbfc057f1e9cdd2522cecb8ce3d2440
GallegoSav <123730578+GallegoSav@users.noreply.github.com>
2026-08-19 14:08:42 +0200

    Replace quad_vec with sp_int.quad_vec for integration

--- cosipy/threeml/custom_functions.py
+++ cosipy/threeml/custom_functions.py
@@ -29,7 +29,6 @@ from astromodels.utils.pretty_list import dict_to_list
 from astromodels.functions import Constant
 from typing import Optional, Dict 
 import scipy.integrate as sp_int
-import scipy.integrate.quad_vec as quad_vec
 import collections
 
 import logging
@@ -660,7 +659,7 @@ class CosipyPointSource(Source, Node):
                         return reentrant_call(x, tag=None)
 
                     # Now integrate
-                    integrals = quad_vec(
+                    integrals = sp_int.quad_vec(
                         integral, a, b, epsrel=1e-5
                     )[0]
 
