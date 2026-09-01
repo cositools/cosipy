f2d14c982bf03eef9830e98419c28d72928e25e3
Israel Martinez <imc@umd.edu>
2026-09-01 10:57:46 -0400

    convenient access for selectors
    Signed-off-by: Israel Martinez <imc@umd.edu>

--- cosipy/event_selection/__init__.py
+++ cosipy/event_selection/__init__.py
@@ -1,1 +1,4 @@
 from .good_time_interval import GoodTimeInterval
+from .distance_selection import DistanceSelector
+from .chain_selector import ChainEventSelectors
+from .time_selection import TimeSelector
